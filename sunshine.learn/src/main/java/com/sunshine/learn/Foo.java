4c39c3825e9aa00b16b9bde6bff05f87e7cbf055
a-ray-of-sunshine <884105526@qq.com>
2015-06-16 13:21:09 +0800

    Foo

---
+++ sunshine.learn/src/main/java/com/sunshine/learn/Foo.java
@@ -0,0 +1,8 @@
+package com.sunshine.learn;
+
+public class Foo {
+
+	public void foo(){
+		System.out.println("com.sunshine.learn");
+	}
+}
---
+++ sunshine.learn/src/test/java/com/sunshine/learn/FooTest.java
@@ -0,0 +1,14 @@
+package com.sunshine.learn;
+
+import org.junit.Test;
+
+
+public class FooTest{
+
+	@Test
+	public void testFoo(){
+		Foo foo = new Foo();
+		foo.foo();
+	}
+	
+}
