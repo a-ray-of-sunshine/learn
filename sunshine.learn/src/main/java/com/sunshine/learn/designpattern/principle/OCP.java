eeffb3fd5b4ff188f2b7f994bc703ad8b0636121
a-ray-of-sunshine <884105526@qq.com>
2015-07-01 21:16:15 +0800

    面向对象开发的基本原则

---
+++ sunshine.learn/src/main/java/com/sunshine/learn/designpattern/principle/OCP.java
@@ -0,0 +1,12 @@
+package com.sunshine.learn.designpattern.principle;
+
+/**
+ * 2015年7月1日21:14:38
+ * 开闭原则
+ * Software entities should be open for extension,but closed for modification
+ */
+public class OCP {
+
+	
+	
+}
