6038058e3c5d4982ba53511db9a2eeb7692698f2
a-ray-of-sunshine <884105526@qq.com>
2015-07-01 22:20:17 +0800

    Update OCP.java

--- sunshine.learn/src/main/java/com/sunshine/learn/designpattern/principle/OCP.java
+++ sunshine.learn/src/main/java/com/sunshine/learn/designpattern/principle/OCP.java
@@ -2,7 +2,7 @@ package com.sunshine.learn.designpattern.principle;
 
 /**
  * 2015年7月1日21:14:38
- * 开闭原则
+ * 开闭原则 --- 关于抽象和封装的原则 --- 对系统的变化点进行抽象和封装
  * Software entities should be open for extension,but closed for modification
  */
 public class OCP {
