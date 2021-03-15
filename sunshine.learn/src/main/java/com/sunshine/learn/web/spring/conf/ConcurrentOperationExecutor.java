a3bc9cce144f5def8b28a4a9cacf0ffb62835d1a
a-ray-of-sunshine <884105526@qq.com>
2021-03-15 10:49:21 +0800

    实现方法访问计数功能

--- sunshine.learn/src/main/java/com/sunshine/learn/web/spring/conf/ConcurrentOperationExecutor.java
+++ sunshine.learn/src/main/java/com/sunshine/learn/web/spring/conf/ConcurrentOperationExecutor.java
@@ -1,37 +1,34 @@
 package com.sunshine.learn.web.spring.conf;
 
+import java.util.concurrent.atomic.AtomicInteger;
+import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.core.Ordered;
-import org.springframework.dao.PessimisticLockingFailureException;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Aspect
 @Component
 public class ConcurrentOperationExecutor implements Ordered {
-    @Override
-    public int getOrder() {
-        return 0;
-    }
 
-    @Pointcut("execution(* com.sunshine.learn.web.spring.service..*.*(..))")
-    public void statOperation() {
-    }
+  private AtomicInteger count = new AtomicInteger();
 
-    @Around("statOperation()")
-    public Object doConcurrentOperation(ProceedingJoinPoint pjp) throws Throwable {
-        int numAttempts = 0;
-        PessimisticLockingFailureException lockFailureException;
-        do {
-            numAttempts++;
-            try {
-                return pjp.proceed();
-            } catch (PessimisticLockingFailureException ex) {
-                lockFailureException = ex;
-            }
-        } while (numAttempts <= 5);
-        throw lockFailureException;
-    }
+  @Override
+  public int getOrder() {
+    return 0;
+  }
+
+  @Pointcut("execution(* com.sunshine.learn.web.spring.service..*.*(..))")
+  public void statOperation() {
+  }
+
+  @Around("statOperation()")
+  public Object doConcurrentOperation(ProceedingJoinPoint pjp) throws Throwable {
+    Object result = pjp.proceed();
+    log.info("access method count: {}", count.incrementAndGet());
+    return result;
+  }
 }
