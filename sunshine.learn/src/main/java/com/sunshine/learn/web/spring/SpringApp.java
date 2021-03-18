d233ca64db8f78ae2bbea4ee880d931fca5ea62d
a-ray-of-sunshine <884105526@qq.com>
2021-03-18 19:45:14 +0800

    启用基于注解的事务

--- sunshine.learn/src/main/java/com/sunshine/learn/web/spring/SpringApp.java
+++ sunshine.learn/src/main/java/com/sunshine/learn/web/spring/SpringApp.java
@@ -9,11 +9,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Slf4j
 @EnableAsync
-@EnableAspectJAutoProxy
 @SpringBootApplication
+@EnableAspectJAutoProxy
+@EnableTransactionManagement
 public class SpringApp {
 
   public SpringApp() {
