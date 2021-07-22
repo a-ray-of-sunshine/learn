5c26c1534d101c299a4b55a24aeb6daad9f5e414
a-ray-of-sunshine <884105526@qq.com>
2021-07-22 10:49:18 +0800

    update

--- sunshine.learn/src/main/java/com/sunshine/learn/algorithm/hw/HJ7.java
+++ sunshine.learn/src/main/java/com/sunshine/learn/algorithm/hw/HJ7.java
@@ -7,14 +7,19 @@ import java.util.Scanner;
  * 取近似值
  */
 public class HJ7 {
+
+    private static final int SCALE = 2;
+
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
 
         double num = scan.nextDouble();
         BigDecimal decimal = new BigDecimal(new Double(num).toString());
-        int intValue = decimal.setScale(0, BigDecimal.ROUND_HALF_UP).intValue();
-        System.out.println(intValue);
+        decimal = decimal.setScale(SCALE, BigDecimal.ROUND_HALF_UP);
+
+        System.out.println(decimal.floatValue());
+        System.out.println(decimal.intValue());
 
         scan.close();
 
